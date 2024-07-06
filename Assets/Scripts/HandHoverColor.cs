d96b708b31d712d04beb6ce4181739c3b0d7c409
Bregaa <matteo.breganni@gmail.com>
2024-07-06 02:55:35 +0200

    Fuse box lever now turns on the lights and locks in position, disabling the grab interactible. Reworked system that changes the material of the hand when it hovers over an interactible (it wasn't working before). Fixed sparks lighting

--- Assets/Scripts/HandHoverColor.cs
+++ Assets/Scripts/HandHoverColor.cs
@@ -7,16 +7,40 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandHoverColor : MonoBehaviour
 {
-    [Tooltip("The colored hand entity")]
-    public GameObject coloredHandController;
+    [Tooltip("The hand entity")]
+    public GameObject handController;
+    [Tooltip("The base material")]
+    public Material baseMaterial;
+    [Tooltip("The material to show on hover")]
+    public Material hoverMaterial;
 
 
-    public void ShowColoredHand()
+    public void HoverRightHand()
     {
-        coloredHandController.SetActive(true);
+        SkinnedMeshRenderer renderer= handController.transform.Find("[Right Controller] Model Parent")?.gameObject.transform.Find("RightHand(Clone)")?.gameObject.transform.Find("hands:hands_geom")?.gameObject.transform.Find("hands:Rhand")?.gameObject.GetComponent<SkinnedMeshRenderer>();
+        List<Material> materials = new List<Material>();
+        materials.Add(hoverMaterial);
+        renderer.SetMaterials(materials);
     }
-    public void HideColoredHand()
+    public void UnhoverRightHand()
     {
-        coloredHandController.SetActive(false);
+        SkinnedMeshRenderer renderer = handController.transform.Find("[Right Controller] Model Parent")?.gameObject.transform.Find("RightHand(Clone)")?.gameObject.transform.Find("hands:hands_geom")?.gameObject.transform.Find("hands:Rhand")?.gameObject.GetComponent<SkinnedMeshRenderer>();
+        List<Material> materials = new List<Material>();
+        materials.Add(baseMaterial);
+        renderer.SetMaterials(materials);
+    }
+    public void HoverLeftHand()
+    {
+        SkinnedMeshRenderer renderer = handController.transform.Find("[Left Controller] Model Parent")?.gameObject.transform.Find("LeftHand(Clone)")?.gameObject.transform.Find("hands:hands_geom")?.gameObject.transform.Find("hands:Lhand")?.gameObject.GetComponent<SkinnedMeshRenderer>();
+        List<Material> materials = new List<Material>();
+        materials.Add(hoverMaterial);
+        renderer.SetMaterials(materials);
+    }
+    public void UnhoverLeftHand()
+    {
+        SkinnedMeshRenderer renderer = handController.transform.Find("[Left Controller] Model Parent")?.gameObject.transform.Find("LeftHand(Clone)")?.gameObject.transform.Find("hands:hands_geom")?.gameObject.transform.Find("hands:Lhand")?.gameObject.GetComponent<SkinnedMeshRenderer>();
+        List<Material> materials = new List<Material>();
+        materials.Add(baseMaterial);
+        renderer.SetMaterials(materials);
     }
 }
---
+++ Assets/Scripts/LightsLeverOn.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LightsLeverOn : MonoBehaviour
+{
+    private GameObject lever;
+    private Rigidbody leverRigidbody;
+    private GameObject knob;
+    private CapsuleCollider knobCollider;
+    private bool areLightsOn;
+
+    private GameObject pilar1;
+    private GameObject pilar2;
+    private GameObject pilar3;
+    private GameObject pilar4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lever = this.gameObject; //Gets the GameObject the script is attached to
+        areLightsOn = false;
+
+        pilar1 = GameObject.Find("Pilar1").transform.Find("LampRack")?.gameObject;
+        pilar2 = GameObject.Find("Pilar2").transform.Find("LampRack")?.gameObject;
+        pilar3 = GameObject.Find("Pilar3").transform.Find("LampRack")?.gameObject;
+        pilar4 = GameObject.Find("Pilar4").transform.Find("LampRack")?.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((!areLightsOn))
+        {
+            Vector3 rotation = lever.transform.localRotation.eulerAngles;
+            if(rotation.x < 280 && rotation.x > 40)
+            {
+                leverRigidbody = lever.GetComponent<Rigidbody>();
+                leverRigidbody.constraints = RigidbodyConstraints.FreezeRotationX;
+
+                areLightsOn = true;
+                pilar1.transform.Find("LampOn")?.gameObject.SetActive(true);
+                pilar1.transform.Find("LampOff")?.gameObject.SetActive(false);
+                pilar2.transform.Find("LampOn")?.gameObject.SetActive(true);
+                pilar2.transform.Find("LampOff")?.gameObject.SetActive(false);
+                pilar3.transform.Find("LampOn")?.gameObject.SetActive(true);
+                pilar3.transform.Find("LampOff")?.gameObject.SetActive(false);
+                pilar4.transform.Find("LampOn")?.gameObject.SetActive(true);
+                pilar4.transform.Find("LampOff")?.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void DisableInteractableFuseBoxLever()
+    {
+        if (areLightsOn)
+        {
+            knobCollider = lever.transform.Find("Knob")?.gameObject.GetComponent<CapsuleCollider>();
+            knobCollider.enabled = false;
+        }
+    }
+}
+//276
+//        
