37ac7c416f6999e004732c6a2c1706545d71476b
Bregaa <matteo.breganni@gmail.com>
2024-07-16 16:04:00 +0200

    Finished cans game. Added last painting

---
+++ Assets/Scripts/CansGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CansGame : MonoBehaviour
+{
+    public GameObject door;
+    public void CansGameWin()
+    {
+        Debug.Log("asd");
+        door.GetComponent<BoxCollider>().enabled = false;
+
+        door.transform.Find("Door").transform.Find("Knob").GetComponent<XRGrabInteractable>().enabled = true;
+        door.transform.Find("Door").GetComponent<Rigidbody>().freezeRotation = false;
+        door.GetComponent<AudioSource>().Play();
+    }
+}
